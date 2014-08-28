673d1ca0b5de829267b75b6b7c0f6c55da468a16
tmerlet <tmerlet@gmail.com>
2014-08-28 23:36:08 +0100

    @playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,77 +3,15 @@ class PlaylistsController < ApplicationController
   # GET /playlists.json
   def index
 
-    #DON'T FORGET THE SINGLE QUOTES AROUND THE JSON HASH
-    playlists_json = ('{
-      "href": "https://api.spotify.com/v1/users/wizzler/playlists",
-      "items": [ {
-        "collaborative": false,
-        "external_urls": {
-          "spotify": "http://open.spotify.com/user/wizzler/playlists/53Y8wT46QIMz5H4WQ8O22c"
-        },
-        "href": "https://api.spotify.com/v1/users/wizzler/playlists/53Y8wT46QIMz5H4WQ8O22c",
-        "id": "53Y8wT46QIMz5H4WQ8O22c",
-        "name": "Wizzlers Big Playlist",
-        "owner": {
-          "external_urls": {
-            "spotify": "http://open.spotify.com/user/wizzler"
-          },
-          "href": "https://api.spotify.com/v1/users/wizzler",
-          "id": "wizzler",
-          "type": "user",
-          "uri": "spotify:user:wizzler"
-        },
-        "public": true,
-        "tracks": {
-          "href": "https://api.spotify.com/v1/users/wizzler/playlists/53Y8wT46QIMz5H4WQ8O22c/tracks",
-          "total": 30
-        },
-        "type": "playlist",
-        "uri": "spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c"
-      }, {
-        "collaborative": false,
-        "external_urls": {
-          "spotify": "http://open.spotify.com/user/wizzlersmate/playlists/1AVZz0mBuGbCEoNRQdYQju"
-        },
-        "href": "https://api.spotify.com/v1/users/wizzlersmate/playlists/1AVZz0mBuGbCEoNRQdYQju",
-        "id": "1AVZz0mBuGbCEoNRQdYQju",
-        "name": "Another Playlist",
-        "owner": {
-          "external_urls": {
-            "spotify": "http://open.spotify.com/user/wizzlersmate"
-          },
-          "href": "https://api.spotify.com/v1/users/wizzlersmate",
-          "id": "wizzlersmate",
-          "type": "user",
-          "uri": "spotify:user:wizzlersmate"
-        },
-        "public": true,
-        "tracks": {
-          "href": "https://api.spotify.com/v1/users/wizzlersmate/playlists/1AVZz0mBuGbCEoNRQdYQju/tracks",
-          "total": 58
-        },
-        "type": "playlist",
-        "uri": "spotify:user:wizzlersmate:playlist:1AVZz0mBuGbCEoNRQdYQju"
-      } ],
-      "limit": 9,
-      "next": null,
-      "offset": 0,
-      "previous": null,
-      "total": 9
-    }')
-
-
-    @playlists = JSON.parse(playlists_json)
-    @playlist = Playlist.new
-
-    # @playlists = Playlist.all
-
     if current_user
       token = current_user.session_token
       auth = "Bearer " + token
-      request = HTTParty.get("https://api.spotify.com/v1/users/#{current_user.uid}/playlists", :headers => { "Authorization" => auth})
-      response_hash = JSON(request)
+      @playlists = HTTParty.get("https://api.spotify.com/v1/users/#{current_user.uid}/playlists", :headers => { "Authorization" => auth})
+      # response_hash = JSON(request)
+      # binding.pry
     end
+
+    @playlist = Playlist.new
 
     respond_to do |format|
       format.html # index.html.erb
