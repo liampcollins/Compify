fa5fecf7b8afd34cc73af43e60d57fb0f07092da
Liam <liampcollins@gmail.com>
2014-08-28 15:57:07 +0100

    pushing rails model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compify::Application.config.secret_token = '196d322ed10fdbff0027d9f1d25896b8fb50d351a1ecd41cf7849ca52861ac923d078a68977faef802a2a1898f022e758d6d778fe0a0c9941c26f754fcec7591'
